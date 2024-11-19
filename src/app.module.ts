83b1fcae36c4748401410fa29d49361e6a10724f
PerfectionGeeks1 <mohammed.f@perfectiongeeks.com>
2024-11-19 15:51:46 +0530

    saas_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MovieModule } from './movie/movie.module';
+
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nestjsdb')
-    ,UserModule, MovieModule],
+    MongooseModule.forRoot('mongodb://localhost/nestjsdb')],
 })
 export class AppModule {}
